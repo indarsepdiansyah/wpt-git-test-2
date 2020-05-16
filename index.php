b8022f158a78f1a82b14a085c330034d0dae6b43
indarsepdiansyah <indarsepdiansyah63@gmail.com>
2020-05-16 22:13:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, voluptate minima quasi eligendi molestias ex?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
